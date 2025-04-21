8654e658ba6c8d80bb1b9bb8d386734e22840eea
salman-aziz-4425 <salmanaziz216@gmail.com>
2025-04-21 13:55:01 +0500

    feat: implement layout fix

--- src/components/ui/multi-step-form.tsx
+++ src/components/ui/multi-step-form.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion"
 
-import { Card } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { useStepStore } from "@/store/use-step-store"
 
